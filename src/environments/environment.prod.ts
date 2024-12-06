3c49f443071165f46e55284dd18eff7010c9a9c8
Alaric Senpai <ckagenou96@gmail.com>
2024-12-06 14:48:56 +0300

    env updates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment ={
     production:true,
-    apiurl: "http://localhost:3500",
+    apiurl: "http://ec2-13-60-182-77.eu-north-1.compute.amazonaws.com:3500",
     jwtToken: 'b4a4cc8ab96492af65fceb746b04b57acfcca6322313606d35d2dc2f893d43e871203e92e20b86178f629fbc0',
         tinymce: "srct5pkbga4oiqsscr7gq7wpsg86phftgmquna3n5ihh5vi2",
     initmce: {
