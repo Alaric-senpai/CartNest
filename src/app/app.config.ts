64fb5cff3a9c6033fbdc5c7ea3ef78764c4a4e8c
Alaric Senpai <ckagenou96@gmail.com>
2024-12-08 19:32:56 +0300

    api utl setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,15 +5,26 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations'; // Import BrowserAnimationsModule
 import { InterceptorService } from './services/interceptor.service';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBJIcuBi78ZVTusZeZ-vq9k4yBiQBmcUbc",
+  authDomain: "cartnest-5cd05.firebaseapp.com",
+  projectId: "cartnest-5cd05",
+  storageBucket: "cartnest-5cd05.firebasestorage.app",
+  messagingSenderId: "310378281684",
+  appId: "1:310378281684:web:59b21a3e996ef56bdb5941"
+};
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    importProvidersFrom(HttpClientModule, BrowserAnimationsModule), // Add BrowserAnimationsModule here
+    importProvidersFrom(HttpClientModule, BrowserAnimationsModule),
+    
     {
       provide: HTTP_INTERCEPTORS,
       useClass: InterceptorService,
       multi: true
-    }
+    },
+    
   ]
 };
